e64795225115f6e923ae3221582a78cdf57acb9f
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-03-01 16:29:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Task 22
+// If you have not recieved in array index error in one of your
+//  programs yet, try to make one happen.
+var newArry = ['will smith', 'James Bond', 'Tem Cruise'];
+//  Accessing arry by index
+console.log(newArry[5]);
+// Make sur you correct the eror before closing the program.
+// Correct the error
+console.log(newArry);
---
+++ main.ts
@@ -0,0 +1,11 @@
+// Task 22
+// If you have not recieved in array index error in one of your
+//  programs yet, try to make one happen.
+let newArry :string []= ['will smith','James Bond', 'Tem Cruise'];
+
+//  Accessing arry by index
+console.log(newArry[5]);
+// Make sur you correct the eror before closing the program.
+// Correct the error
+console.log(newArry);
+
